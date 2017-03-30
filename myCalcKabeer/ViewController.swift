038f636db551c5b68f86462d47c798c838463449
kabeer1110 <Kabeer@Kabeers-MBP.wireless.newschool.edu>
2017-03-30 14:44:00 -0400

    Final Changes for the Day

--- myCalcKabeer/ViewController.swift
+++ myCalcKabeer/ViewController.swift
@@ -7,8 +7,22 @@
 //
 
 import UIKit
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+
+}
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var calcLabel: UILabel!
+    
+    var labelString: String = "0"
+    var currentMode:modes = .not_set
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +33,51 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+  
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        if (lastButtonWasMode){
+            lastButtonWasMode = false
+            labelString = "0"
+        }
+        
+        let stringValue:String? = sender.titleLabel?.text
+        labelString = labelString.appending(stringValue!)
+        
+        calcLabel.text = labelString
+         updateText()
+        
+    }
 
+    @IBAction func didPressPlus(_ sender: UIButton) {
+        changeMode(newMode: .addition)
+    }
 
+    @IBAction func didPressClear(_ sender: UIButton) {
+        
+        labelString = "0"
+        currentMode = .not_set
+        savedNum = 0
+        lastButtonWasMode = false
+        updateText()
+        
+    }
+    
+    @IBAction func didPressEquals(_ sender: UIButton) {
+
+    }
+    
+    func updateText(){
+        
+        guard let calcLabelInt:Int = Int(labelString) else{return}
+        if (currentMode == .not_set) {savedNum = calcLabelInt}
+        calcLabel.text = "\(calcLabelInt)"
+    }
+    
+    func changeMode(newMode:modes){
+        if (savedNum == 0) {return}
+        currentMode = newMode
+        lastButtonWasMode = true
+    }
 }
 
